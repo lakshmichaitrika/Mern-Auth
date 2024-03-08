1d52b818e2ab7ad15d7cd3728cb3bccf1142a757
chaitrika <lakshmichaitrika2002@gmail.com>
2024-03-08 13:40:23 +0530

    create and run server

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className='bg-gray-100 p-3 flex justify-between items-center'>
+    <div className='bg-gray-100'>
+    <div className='p-3 flex justify-between items-center max-w-6xl mx-auto'>
     <Link to="/">
     <h1 className='text-xl font-normal'>Auth App</h1>
     </Link>
    
-     <ul className='flex justify-around'>
+     <ul className='flex justify-around gap-4'>
         <Link  to="/">
-        <li className='mr-3'>Home</li>
+        <li>Home</li>
         </Link>
         <Link to="/about">
-        <li className='mr-3'>About</li>
+        <li >About</li>
         </Link>
        <Link to="/SignIn">
-       <li className='mr-3'>SignIn</li>
+       <li>SignIn</li>
        </Link>
         
      </ul>
+
+    </div>
+  
     </div>
   )
 }
