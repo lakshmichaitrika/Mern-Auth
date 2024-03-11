80815970860e707010cd1ccb111e0241e2770a5d
chaitrika <lakshmichaitrika2002@gmail.com>
2024-03-11 19:00:40 +0530

    signin api functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import jwt  from "jsonwebtoken"
 import { errorHandler } from "../utils/error.js"
 
 export const signup=async (req,res,next)=>{
@@ -15,7 +16,33 @@ export const signup=async (req,res,next)=>{
     }
     catch(e){
         console.log(e)
-       next({message:"something went wrong"})
+       next(errorHandler(404,"you are already signup"))
+
+    }
+    
+}
+
+export const signin=async (req,res,next)=>{
+    console.log(req.body)
+    const {password,email}=req.body
+     try{
+        const validUser=await User.findOne({email})
+       
+        if (!validUser) return next(errorHandler(401,"user not found"))
+        const validPassword=bcryptjs.compareSync(password,validUser.password)
+        if (!validPassword) return next(errorHandler(401,"invalid login"))
+        const token=jwt.sign({id:validUser._id},process.env.JWT_TOKEN)
+
+        const {password:hashedPassword,...rest}=validUser._doc
+        console.log(hashedPassword)
+        res.cookie('token',token,{httpOnly:true,expires:new Date(Date.now()+360000)})
+        .status(200)
+        .json(rest)
+          }
+        
+    catch(e){
+        console.log(e)
+       next(e)
 
     }
     
