4a30a441180dca7094aa4a27cddb26535d38a08c
chaitrika <lakshmichaitrika2002@gmail.com>
2024-03-17 16:44:51 +0530

    redux

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,66 +1,78 @@
-import React, { useState } from "react";
-import { Link ,useNavigate} from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SignIn() {
- const [formData,setForm]=useState({})
-// const [error,setError]=useState("") 
- const [loading,loadingState]=useState(false) 
-const navigate=useNavigate();
-  const handleChange=(e)=>{
-    setForm({...formData,[e.target.id]:e.target.value})
-  }
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
 
-  const handleSubmit=async(e)=>{
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    loadingState(true)
-    const res=await fetch("/api/auth/signin",
-    {
-      method:'POST',
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(formData)
-      
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error));
     }
-    )
-    loadingState(false)
-    const data=await res.json();
-    console.log(res)
-    
-    navigate('/')
-  }
-
-  
+  };
   return (
-    <div className="max-w-lg mx-auto p-5">
-      <h1 className="text-3xl text-center font-semibold my-6">Sign In</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-       
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="email"
-          placeholder="Email"
-          id="email"
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
           onChange={handleChange}
-          className="bg-slate-100 p-3 rounded-lg"
         />
         <input
-          type="password"
-          placeholder="Password"
-          id="password"
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
           onChange={handleChange}
-          className="bg-slate-100 p-3 rounded-lg"
         />
-        <button type="submit" className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-         {loading?"Loading...":"Sign In"}
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
-      <div className="flex gap-2 mt-5">
-        <p>Create an account</p>
-        <Link to="/signup">
-        <span className="text-blue-500">Sign Up</span>
-      
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-500'>Sign up</span>
         </Link>
-       </div>
+      </div>
+      <p className='text-red-700 mt-5'>
+        {error ? error.message || 'Something went wrong!' : ''}
+      </p>
     </div>
   );
 }
